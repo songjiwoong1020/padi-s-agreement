42296570a8c305a33f55d1f76d8ee98eadec04a2
JiwoongSong <songjiwoong1020@gmail.com>
2025-04-25 23:14:54 +0900

    내용추가

---
+++ src/app/2/_component/Group.tsx
@@ -0,0 +1,82 @@
+//page를 나타내기 위한 해당 page 최상위 컴포넌트
+'use client';
+
+import Wrap from "@/app/_common/Wrap";
+import Button from "@/app/_common/Button";
+import Content1 from "./Content1";
+import Content2 from "./Content2";
+import { useRef } from "react";
+import { useRouter } from 'next/navigation';
+
+const titles = {
+  "title1": "PADI Discover Scuba® Diving 수강생 선언문",
+  "title2": "초상권 면책 동의서",
+  "title3": "개인정보 제공 및 이용 동의서"
+}
+
+//type1 event bubbling 
+// export default function Group() {
+//   const checkRef1 = useRef<HTMLInputElement>(null);
+//   const checkRef2 = useRef<HTMLInputElement>(null);
+//   const buttonRef = useRef<HTMLButtonElement>(null);
+
+//   const handleChange = () => {
+//     (checkRef1?.current?.checked && checkRef2?.current?.checked) 
+//     ? buttonRef.current?.removeAttribute('disabled')
+//     : buttonRef.current?.setAttribute('disabled', 'true');
+//   }
+//
+//   return (
+//     <div onChange={handleChange}>
+//       <Wrap title={titles.title1} >
+//         <Content1 />
+//       </Wrap>
+//       <hr/>
+//       <Wrap title={titles.title2} checkRef={checkRef1}>
+//         <Content2 />
+//       </Wrap>
+//       <hr/>
+//       <Wrap title={titles.title3} checkRef={checkRef2}>
+//         <Content3 />
+//       </Wrap>
+//       <Button text={'다음'} buttonRef={buttonRef}/>
+//     </div>
+//   )
+// }
+
+//type2 handler prop 
+export default function Group() {
+  const checkRef1 = useRef<HTMLInputElement>(null);
+  const checkRef2 = useRef<HTMLInputElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const router = useRouter();
+
+  const handleChange = () => {
+    (checkRef1?.current?.checked && checkRef2?.current?.checked) 
+    ? buttonRef.current?.removeAttribute('disabled')
+    : buttonRef.current?.setAttribute('disabled', 'true');
+  }
+
+  const handleClick = () => {
+    router.push('/page2');
+  }
+
+  return (
+    <>
+      <Wrap title={titles.title1} >
+        <Content1 />
+      </Wrap>
+      <hr/>
+      <Wrap
+        title={titles.title2}
+        showCheckbox
+        checkRef={checkRef1}
+        handleChange={handleChange}
+      >
+        <Content2 />
+      </Wrap>
+      <hr/>
+      <Button text={'다음'} buttonRef={buttonRef} handleClick={handleClick}/>
+    </>
+  )
+}
--- src/app/2/page.tsx
+++ src/app/2/page.tsx
@@ -1,22 +1,9 @@
-import { page2_title1, page2_title2 } from "../_asset/titles.json";
-import Wrap from "../_common/_wrap";
-import Button from "../_common/Button";
-import Input from "../_common/Input";
-import Page2_content1 from "../_asset/page2content1";
-import Page2_content2 from "../_asset/page2content2";
+import Group from "./_component/Group";
 
-export default function Page2() {
+export default function Page() {
   return (
-    <div>
-      <Wrap title={page2_title1} content={<Page2_content1/>} useAgree={false}/>
-      <hr/>
-      <Wrap title={page2_title2} content={<Page2_content2/>} useAgree={false}/>
-      <Input label="영문이름" type="text"/>
-      <Input label="휴대폰번호" type="tel"/>
-      <Input label="E-MAIL" type="email"/>
-      <Input label="생년 월 일" type="date"/>
-      <Input label="주소" type="text"/>
-      {/* <Button text="다음" disabled={false} onclick={()=>{}}/> */}
-    </div>
-  );
+    <>
+      <Group/>
+    </>
+  )
 }
