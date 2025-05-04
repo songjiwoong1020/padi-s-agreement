8c87548c67decb376b0df72b7e38b298e8aae1df
JiwoongSong <songjiwoong1020@gmail.com>
2025-05-04 23:57:39 +0900

    코드 수정

--- src/app/_common/button.tsx
+++ src/app/_common/button.tsx
@@ -3,17 +3,15 @@ import styles from "./button.module.css";
 type props = {
   text: string,
   buttonRef: React.Ref<HTMLButtonElement>,
-  isDisabled: boolean,
   handleClick: React.MouseEventHandler<HTMLButtonElement>
 }
 
 export default function Button({
   text,
   buttonRef,
-  isDisabled,
   handleClick}: props) {
   return (
-    <button className={styles.btn} disabled={isDisabled} ref={buttonRef} onClick={handleClick}>{text}</button>
+    <button className={styles.btn} disabled={true} ref={buttonRef} onClick={handleClick}>{text}</button>
     //https://velog.io/@eenaree/props-onClick-type 해당내용 참고.
   );
 }
